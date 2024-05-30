eb905541dbbbf8d01b8a9722197433e769f4b449
JotaveX <jvpiloni@gmail.com>
2024-05-30 12:08:23 -0300

    Fix: Cache db connection

--- config/app-options.constants.ts
+++ config/app-options.constants.ts
@@ -1,19 +1,18 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
 import { CacheModuleAsyncOptions } from "@nestjs/cache-manager";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { redisStore } from "cache-manager-redis-store";
+
 
 export const RedisOptions: CacheModuleAsyncOptions = {
   isGlobal: true,
   imports: [ConfigModule],
-  useFactory: async () => {
-    const store = await redisStore({
-      socket: {
-        host: process.env.REDIS_HOST,
-        port: parseInt(process.env.REDIS_PORT)
-      },
-    });
+  useFactory: async (configService: ConfigService) => {
     return {
-      store: () => store,
+      store: redisStore,
+      host: configService.get('REDIS_HOST'),
+      port: configService.get<number>('REDIS_PORT'),
+      auth_pass: configService.get('REDIS_PASSWORD'),
+      ttl: 300, // Default TTL (time-to-live) for cache
     };
   },
   inject: [ConfigService],
