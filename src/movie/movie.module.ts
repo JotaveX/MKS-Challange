00d6aa96be7fd64803bfba4ca0caccf3d0f7a350
JotaveX <jvpiloni@gmail.com>
2024-05-31 00:14:10 -0300

    Mod: movie module

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -9,14 +9,7 @@ import { RedisOptions } from 'src/config/app-options.constants';
 import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-imports: [PrismaModule, AuthModule, UserModule, CacheModule.registerAsync(RedisOptions),
-  CacheModule.register({
-    isGlobal: true,
-    store: redisStore,
-    host: process.env.REDIS_HOST,
-    port: parseInt(process.env.REDIS_PORT),
-    auth_pass: process.env.REDIS_PASSWORD,
-  })
+imports: [PrismaModule, AuthModule, UserModule, CacheModule.registerAsync(RedisOptions)
 ],
   controllers: [MovieController],
   providers: [MovieService],
